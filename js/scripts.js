5c713c5d1251c67ec8279a6c218e8172d3659395
Natusechka <natusechka.kravtsova@gmail.com>
2020-04-25 10:56:46 +0300

    start window

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,21 @@ $(document).ready(function(){
             }
         }
     });
+    $( function() {
+        $( "#dialog" ).dialog({
+            autoOpen: false,
+            show: {
+                effect: "blind",
+                duration: 1000
+            },
+            hide: {
+                effect: "explode",
+                duration: 1000
+            }
+        });
+
+        $( "#opener" ).on( "click", function() {
+            $( "#dialog" ).dialog( "open" );
+        });
+    } );
 });
